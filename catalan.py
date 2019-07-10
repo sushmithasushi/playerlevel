94a9384610889d32377fd0ef387d1e63b64d6b52
reshmahayathbi1999 <50619287+reshmahayathbi1999@users.noreply.github.com>
2019-07-10 19:39:29 +0530

    Create catalan.py

---
+++ catalan.py
@@ -0,0 +1,10 @@
+n=int(input())
+def catalan(n): 
+    if n <= 1 : 
+        return 1
+    res = 0
+    for i in range(n): 
+        res += catalan(i) * catalan(n-i-1) 
+    return res 
+for i in range(n+1): 
+    print(catalan(i),end=" ") 
