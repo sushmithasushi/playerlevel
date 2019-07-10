fe6b226664be084c806f757c51249cec1b7654c8
reshmahayathbi1999 <50619287+reshmahayathbi1999@users.noreply.github.com>
2019-07-10 20:20:52 +0530

    Create matrixproduct.py

---
+++ matrixproduct.py
@@ -0,0 +1,10 @@
+num1=int(input())
+arr=[list(map(int,input().split(" "))) for i in range(num1)]
+i,j=0,len(arr)-1
+a,b=0,0
+while i<len(arr):
+    a+=arr[i][i]
+    b+=arr[i][j]
+    i+=1
+    j-=1
+print(a*b)
