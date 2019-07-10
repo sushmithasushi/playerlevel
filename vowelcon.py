a7248119e4c93b69f84850587c7cc1b582862cb9
reshmahayathbi1999 <50619287+reshmahayathbi1999@users.noreply.github.com>
2019-07-10 20:45:46 +0530

    Create vowelcon.py

---
+++ vowelcon.py
@@ -0,0 +1,7 @@
+astri=input()
+for i in astri:
+    if i=="a" or i=="e" or i=="i" or i=="o" or i=="u":
+        print(i,end="")
+for i in astri:
+    if i!="a" and i!="e" and i!="i" and i!="o" and i!="u":
+        print(i,end="")
