8e9b94b47e7fcee302d35937d0b894214b5b1dcd
sushmithasushi <49743221+sushmithasushi@users.noreply.github.com>
2019-07-16 09:47:50 +0530

    Create setp45.py

---
+++ setp45.py
@@ -0,0 +1,13 @@
+l,s=map(int,input().split())
+l=l/2
+f=0
+for i in range(max(l,s)):
+    for j in range(max(l,s)):
+        if i+j==l and i*j==s:
+            f=1
+            break
+if f==1:
+    print("YES")
+else:
+    print("NO")
+    
